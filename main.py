cad71fcb009ce748e359dec51cd02df8be2c7725
karisa <falcomdolatabadii@gmail.com>
2024-11-10 17:45:28 +0330

    ------

---
+++ main.py
@@ -0,0 +1,9 @@
+a=int(input('enter number'))
+if 0<=a<10 :
+    print('mardod')
+elif 11<a<15:
+    print('ghabol')
+elif 16<a<=20:
+    print('danesh amoz bartar')
+else:
+    print('you can entrer 0 _ 20')
